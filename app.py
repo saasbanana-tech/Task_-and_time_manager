d6c84e2a6a63c71f52d423b4c0ac49d18fe9acbc
anikettyadavvv <aniket.yadav@saasbanana.com>
2026-01-08 08:59:22 +0530

    Connected database via SQLalcchemy

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task_manager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(20), default='Open') 
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    
+
+
 
 @app.route("/")
 def hello_world():
